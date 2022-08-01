7e22f93ae7b2effffa100189a662cfb5bdbdea9c
Ggm-gijoo <gijoo1012@gmail.com>
2022-08-01 18:21:35 +0900

    역 떠나기 버튼

---
+++ Assets/01.Script/Button/ExitButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject exitPanel;
+
+    private void Start()
+    {
+        exitPanel.SetActive(false);
+    }
+
+    public void OnClickExitBtn()
+    {
+        exitPanel.SetActive(true);
+    }
+}
