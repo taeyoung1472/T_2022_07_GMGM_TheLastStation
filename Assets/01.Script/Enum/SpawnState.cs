04a947dc783f6c89dd987a4d4e5acce0e50e429b
LAPTOP_TAEYOUNG\69hop <l32590838@gmail.com>
2022-09-21 18:35:06 +0900

    1차 병합

---
+++ Assets/01.Script/Enum/SpawnState.cs
@@ -0,0 +1,20 @@
+public enum SpawnState
+{
+    CORPSE = 0,
+    ITEMBOX,
+    RUIN
+}
+
+public enum CorpseState
+{
+    NONE = 0,
+    BASIC,
+    ROTTEN
+}
+
+public enum RuinState
+{
+    NONE = 0,
+    BUILDING,
+    IRON
+}
