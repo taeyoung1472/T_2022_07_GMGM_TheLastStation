bcf82c0f722b290d65561d1f9799198f4ddd898b
Ggm-gijoo <gijoo1012@gmail.com>
2022-10-05 23:35:08 +0900

    방추가

---
+++ Assets/01.Script/Enviroment/RoomObject/ToolBox.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBox : Shelf
+{
+    List<ItemDataSO> list = new List<ItemDataSO>();
+
+    public override void Effect()
+    {
+        foreach (var dt in ItemDataManagement.Dict[ItemCategory.Tool].DTS)
+        {
+            list.Add(dt);
+        }
+        for (int i = 0; i < Random.Range(2, 6); i++)
+            InventoryHandler.Instance.Add(list[Random.Range(0, list.Count)]);
+    }
+
+    private void Start()
+    {
+        Effect();
+    }
+}
